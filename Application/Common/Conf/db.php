8cdbabe62169e4cc82586101342160132d196ef6
jameswei <jamesweil2015@gmail.com>
2017-08-31 20:47:50 +0800

    uploads

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -2,11 +2,10 @@
 /* 数据库配置 */
 return array(
     'DB_TYPE' => 'mysql', //数据库类型
-    'DB_HOST' => '127.0.0.1', //数据库主机
-    //'DB_NAME' => 'shopsn_sql', //数据库名称
-    'DB_NAME' => 'shop_test', //数据库名称
-    'DB_USER' => 'root', //数据库用户名
-    'DB_PWD'  => 'root', //数据库密码
+    'DB_HOST' => '172.30.240.195', //数据库主机、
+    'DB_NAME' => 'eshopdb', //数据库名称
+    'DB_USER' => 'eshop', //数据库用户名
+    'DB_PWD'  => 'edward', //数据库密码
     'DB_PORT' => '3306', //数据库端口
     'DB_PREFIX' => 'db_', //数据库前缀
     'DB_CHARSET'=> 'utf8', // 字符集
